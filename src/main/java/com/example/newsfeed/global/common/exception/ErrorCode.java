4502384a6319f756ac42e48c4fcdcf04a4e11580
Roloya28 <choej9742@naver.com>
2025-02-17 19:26:31 +0900

    Docs: 에러 수정

--- src/main/java/com/example/newsfeed/global/common/exception/ErrorCode.java
+++ src/main/java/com/example/newsfeed/global/common/exception/ErrorCode.java
@@ -13,7 +13,9 @@ public enum ErrorCode {
     FILE_NOT_FOUND("FileNotFound", "파일을 찾을 수 없습니다."),
     PASSWORD_SAME_AS_OLD("PasswordSameAsOld", "이전 패스워드와 동일할 수 없습니다."),
 
-    // 댓글용 코드
+    /**
+     * 댓글용 에러 코드
+     */
     FEED_NOT_FOUND("FeedNotFound", "해당하는 게시글을 찾을 수 없습니다."),
     COMMENT_NOT_FOUND("CommentNotFound", "해당하는 댓글을 찾을 수 없습니다."),
     UNAUTHORIZED("Unauthorized", "권한이 없습니다.");
