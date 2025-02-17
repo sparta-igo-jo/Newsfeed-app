6f4de3c194488d9245c2404f9270ac657356dc48
lamgak12 <tntnqk12@naver.com>
2025-02-17 14:11:39 +0900

    feat: 예외 추가

---
+++ src/main/java/com/example/newsfeed/like/exception/CannotFollowSelfException.java
@@ -0,0 +1,12 @@
+package com.example.newsfeed.like.exception;
+
+import com.example.newsfeed.global.common.exception.BaseException;
+import com.example.newsfeed.global.common.exception.ErrorDetail;
+
+import java.util.List;
+
+public class CannotFollowSelfException extends BaseException {
+    public CannotFollowSelfException(List<ErrorDetail> errorDetails) {
+        super(errorDetails);
+    }
+}
