1a8f71d8c56622f80aa86c0786666c6b99c3e8a4
lamgak12 <tntnqk12@naver.com>
2025-02-17 17:03:38 +0900

    Feat: 에러코드 추가

--- src/main/java/com/example/newsfeed/global/common/exception/ErrorCode.java
+++ src/main/java/com/example/newsfeed/global/common/exception/ErrorCode.java
@@ -11,7 +11,9 @@ public enum ErrorCode {
     INVALID_PASSWORD("InvalidPassword", "패스워드가 올바르지 않습니다."),
     UPLOAD_FAILED("UploadFailed", "파일 업로드에 실패했습니다."),
     FILE_NOT_FOUND("FileNotFound", "파일을 찾을 수 없습니다."),
-    PASSWORD_SAME_AS_OLD("PasswordSameAsOld", "이전 패스워드와 동일할 수 없습니다.");
+    PASSWORD_SAME_AS_OLD("PasswordSameAsOld", "이전 패스워드와 동일할 수 없습니다."),
+
+    CANNOT_FOLLOW_SELF("CannotFollowSelf","자기 자신을 팔로우할 수 없습니다.");
 
     private final String code;
     private final String message;
