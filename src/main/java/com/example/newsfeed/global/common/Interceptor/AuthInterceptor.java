73a76329492628331d05ddf7ebf78080b4cf4fc2
cheoni5417 <cheoni5417@naver.com>
2025-02-17 09:12:04 +0900

    Feat : 로그인 인터셉터 생성

---
+++ src/main/java/com/example/newsfeed/global/common/Interceptor/AuthInterceptor.java
@@ -0,0 +1,34 @@
+package com.example.newsfeed.global.common.Interceptor;
+
+import com.example.newsfeed.auth.exception.UserNotLoginException;
+import com.example.newsfeed.global.common.SessionConst;
+import com.example.newsfeed.global.common.exception.ErrorDetail;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.util.List;
+
+import static com.example.newsfeed.global.common.exception.ErrorCode.USER_NOT_LOGIN;
+
+public class AuthInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(
+        HttpServletRequest request,
+        HttpServletResponse response,
+        Object handler
+    ) {
+        HttpSession session = request.getSession(false);
+
+        if(session == null || session.getAttribute(SessionConst.LOGIN_USER) == null) {
+            // TODO : GlobalExceptionHandler 에서 예외처리 필요
+            throw new UserNotLoginException(List.of(
+                    new ErrorDetail(USER_NOT_LOGIN, null, USER_NOT_LOGIN.getMessage())
+            ));
+        }
+
+        return true;
+    }
+}
---
+++ src/main/java/com/example/newsfeed/global/common/SessionConst.java
@@ -0,0 +1,5 @@
+package com.example.newsfeed.global.common;
+
+public class SessionConst {
+    public static final String LOGIN_USER = "loginUser";
+}
