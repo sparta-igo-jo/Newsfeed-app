503a636a4d83cb97732bcf266c4bf272a4e354d0
lamgak12 <tntnqk12@naver.com>
2025-02-18 10:28:53 +0900

    Feat: 좋아요 기능 추가

---
+++ src/main/java/com/example/newsfeed/like/controller/LikeController.java
@@ -0,0 +1,24 @@
+package com.example.newsfeed.like.controller;
+
+import com.example.newsfeed.global.common.Const.SessionConst;
+import com.example.newsfeed.global.response.Response;
+import com.example.newsfeed.like.application.service.LikeService;
+import com.example.newsfeed.like.dto.LikeResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/feeds/{feedId}/like")
+@RequiredArgsConstructor
+public class LikeController {
+    private final LikeService likeService;
+
+    @PostMapping
+    public Response<LikeResponseDto> toggleLike(
+            @PathVariable Long feedId,
+            @SessionAttribute(name = SessionConst.LOGIN_USER) Long sessionUserId
+    ){
+        likeService.toggleLike(sessionUserId, feedId);
+        return Response.of(null);
+    }
+}
---
+++ src/main/java/com/example/newsfeed/like/dto/LikeResponseDto.java
@@ -0,0 +1,11 @@
+package com.example.newsfeed.like.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class LikeResponseDto {
+    private Long feedId;
+    private boolean liked;
+}
